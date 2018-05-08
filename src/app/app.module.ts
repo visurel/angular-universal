02b399e5ae33f24247823c5369ca5ae69c9df147
Steven Enten <steven@enten.fr>
2018-05-08 17:34:18 +0200

    setup @angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
 
 export const APP_ID = 'my-app';
 
@@ -10,7 +13,9 @@ export const APP_ID = 'my-app';
     AppComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: APP_ID })
+    BrowserModule.withServerTransition({ appId: APP_ID }),
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [ AppComponent ]
