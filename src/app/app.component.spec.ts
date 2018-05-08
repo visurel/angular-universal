cb854b1328143f6366092bc9f8b3b0147b3bd9f6
Steven Enten <steven@enten.fr>
2018-05-08 18:40:05 +0200

    update AppComponent with material components

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,17 @@
 import { TestBed, async } from '@angular/core/testing';
+
+import { MaterialModule } from './material.module';
 import { AppComponent } from './app.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        MaterialModule
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
