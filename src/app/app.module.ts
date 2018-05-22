16f6b130a0b178002982e3ad1cef70b0805f87a5
visurel <martin@visurel.com>
2018-05-22 23:13:35 +0200

    chore: add example for i18n error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, TRANSLATIONS, LOCALE_ID } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
 export const APP_ID = 'my-app';
+
+declare const require: any; // maybe required, maybe not
 
 @NgModule({
   declarations: [
@@ -12,7 +14,17 @@ export const APP_ID = 'my-app';
   imports: [
     BrowserModule.withServerTransition({ appId: APP_ID })
   ],
-  providers: [],
+  providers: [
+    {
+      provide: TRANSLATIONS,
+      useFactory: (locale) => {
+        locale = locale || 'en'; // default to english if no locale is provided
+        // return require(`raw-loader!../i18n/messages.${locale}.xlf`); <= should be this, but it throws an error that raw-loader is not found
+        return require(`../i18n/messages.${locale}.xlf`); // same error happening as in issue even without raw-loader, although we probably will need it to load this file
+      },
+      deps: [LOCALE_ID]
+    }
+  ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
